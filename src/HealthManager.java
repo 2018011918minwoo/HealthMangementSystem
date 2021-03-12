20008e00990bf12fb146bf89e0fd92e96800ea00
2018011918minwoo <whalsdn0215@naver.com>
2021-03-13 01:49:18 +0900

    removed hello.java added HealthManager.java

---
+++ src/HealthManager.java
@@ -0,0 +1,13 @@
+
+public class HealthManager {
+
+	public static void main(String[] args) {
+		System.out.print("1. Add Exercise\n");
+		System.out.print("2. Delete Exercise\n");
+		System.out.print("3. Edit Exercise\n");
+		System.out.print("4. View Exercise\n");
+		System.out.print("5. Exit");
+
+	}
+
+}
