361405b3884c03127fe7916afed5066e3c47f5eb
2018011918minwoo <whalsdn0215@naver.com>
2021-03-28 18:34:46 +0900

    addexercise method

--- src/HealthManager.java
+++ src/HealthManager.java
@@ -1,7 +1,72 @@
 import java.util.Scanner;
 
 public class HealthManager {
+	public static void addexercise() {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Exercise Part: ");
+		String part = input.next();
+		if(part.equals("back")) {
+			System.out.print("Exericse: ");
+			String back1 = input.next();
+			System.out.print("Set*Reps");
+			String back2 = input.next();
+			System.out.print("weight");
+			String back3 = input.next();
+		}
+		else if(part.equals("chest")) {
+			System.out.print("Exericse: ");
+			String chest1 = input.next();
+			System.out.print("Set*Reps");
+			String chest2 = input.next();
+			System.out.print("weight");
+			String chest3 = input.next();
+		}
+		else if(part.equals("legs")) {
+			System.out.print("Exericse: ");
+			String legs1 = input.next();
+			System.out.print("Set*Reps");
+			String legs2 = input.next();
+			System.out.print("weight");
+			String legs3 = input.next();
+		}
+		else if(part.equals("arms)")) {
+			System.out.print("Exericse: ");
+			String arms1 = input.next();
+			System.out.print("Set*Reps");
+			String arms2 = input.next();
+			System.out.print("weight");
+			String arms3 = input.next();
+		}
+		else if(part.equals("shoulders")) {
+			System.out.print("Exericse: ");
+			String shoulders1 = input.next();
+			System.out.print("Set*Reps");
+			String shoulders2 = input.next();
+			System.out.print("weight");
+			String shoulders3 = input.next();
+		}
 
+	}
+	public static void deleteexercise() {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Exercise Part: ");
+		String part1 = input.next();
+	}
+	public static void editexercise() {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Exercise Part: ");
+		String part = input.next();
+	}
+	public static void viewexercise() {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Exercise Part: ");
+		String part = input.next();
+	}
+	
 	public static void main(String[] args) {
 		int num = 0;
 		Scanner input = new Scanner(System.in);
@@ -17,63 +82,20 @@ public class HealthManager {
 			num = input.nextInt();
 			
 			
-			switch (num) {
-			case 1:
-				System.out.print("Exercise Part: ");
-				String part = input.next();
-				switch (part) {
-				case "back":
-					System.out.print("Exericse: ");
-					String back1 = input.next();
-					System.out.print("Set*Reps");
-					String back2 = input.next();
-					System.out.print("weight");
-					String back3 = input.next();
-					break;
-				case "chest":
-					System.out.print("Exericse: ");
-					String chest1 = input.next();
-					System.out.print("Set*Reps");
-					String chest2 = input.next();
-					System.out.print("weight");
-					String chest3 = input.next();
-					break;
-				case "legs":
-					System.out.print("Exericse: ");
-					String legs1 = input.next();
-					System.out.print("Set*Reps");
-					String legs2 = input.next();
-					System.out.print("weight");
-					String legs3 = input.next();
-					break;
-				case "arms":
-					System.out.print("Exericse: ");
-					String arms1 = input.next();
-					System.out.print("Set*Reps");
-					String arms2 = input.next();
-					System.out.print("weight");
-					String arms3 = input.next();
-					break;
-				case "shoulders":
-					System.out.print("Exericse: ");
-					String shoulder1 = input.next();
-					System.out.print("Set*Reps");
-					String shoulder2 = input.next();
-					System.out.print("weight");
-					String shoulder3 = input.next();
-					break;
-				}
-				
-				
-			case 2:
-				System.out.print("Exercise Part: ");
-				String part1 = input.next();
-			case 3:
-				System.out.print("Exercise Part: ");
-				String part2 = input.next();
-			case 4:
-			System.out.print("Exercise Part: ");
-			String part3 = input.next();
+			if (num ==1) {
+				addexercise();
+			}
+			else if (num ==2) {
+				deleteexercise();
+			}
+			else if (num ==3) {
+				editexercise();
+			}
+			else if (num ==4) {
+				viewexercise();
+			}
+			else {
+				continue;
 			}
 		}
 	}
